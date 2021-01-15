4204e1de30e17e8225c4bdf4860232caf8e95bb5
Varad <vaidyavarad2001@gmail.com>
2021-01-15 21:59:27 +0530

    created class template file for UR sim

---
+++ UR5.py
@@ -0,0 +1,15 @@
+import pybullet as p
+import pybullet_data
+import numpy as np
+
+
+'''
+Define a class for the UR5 pybullet simulator
+
+NOTE: https://gramaziokohler.github.io/compas_fab/latest/examples/03_backends_ros/07_ros_create_urdf_ur5_with_measurement_tool.html
+
+the above link has a way to mfg custuom UDRF file format
+'''
+
+
+
