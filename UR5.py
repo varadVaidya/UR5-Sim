f43600c5cc42fc1c41825d3d7b82620b7b1374ad
Varad <vaidyavarad2001@gmail.com>
2021-02-04 08:34:29 +0530

    added GUI parameters and analytical jacobian map

--- UR5.py
+++ UR5.py
@@ -40,13 +40,24 @@ class UR5manipulator():
         print(self.controlableJoints)
         self.endEffectorLink = self.controlableJoints[-1]
         
+    def turnOFFJointControl(self):
+        p.setJointMotorControlArray(self.robotID,self.controlableJoints,p.VELOCITY_CONTROL,forces = [0]*6)
     
     def setJointAngles(self,jointAngles):
         zero = [0]*6 
-        p.setJointMotorControlArray(self.robotID,self.controlableJoints,controlMode = p.POSITION_CONTROL ,targetPositions = jointAngles,targetVelocities = zero)
+        p.setJointMotorControlArray(self.robotID,self.controlableJoints,controlMode = p.POSITION_CONTROL ,targetPositions = jointAngles,targetVelocities = zero,
+                                    positionGains = [1]*6,velocityGains = [1]*6)
         
         for __ in range(200):
             p.stepSimulation()
+            
+    def getJointStates(self):
+        joint_states = p.getJointStates(self.robotID, self.controlableJoints)
+        joint_positions = [state[0] for state in joint_states]
+        joint_velocities = [state[1] for state in joint_states]
+        joint_torques = [state[3] for state in joint_states]
+        
+        return joint_positions, joint_velocities, joint_torques
                
                 
     def getForwardKinematics(self):
@@ -66,7 +77,7 @@ class UR5manipulator():
         return jointAngles
         
     def calculateJacobian(self,jointNagles):
-        endEffectorState = p.getLinkState(self.robotID,self.endEffectorLink)
+        endEffectorState = p.getLinkState(self.robotID,self.endEffectorLink,computeForwardKinematics=True)
         
         zero = [0]*6
         
@@ -75,8 +86,30 @@ class UR5manipulator():
         jacobianLinear = np.array(jacobianLinear)
         jacobianRotational = np.array(jacobianRotational)
         
-        return np.vstack((jacobianRotational,jacobianLinear))
+        return np.vstack((jacobianLinear,jacobianRotational))
     
+    def calculateAnalyticalVelocityMap(self):
+        '''
+        will retuen A matrix as required by the HandBook of Robotics:
+        
+        '''
+        endEffector = p.getLinkState(self.robotID,self.endEffectorLink)
+        quat = endEffector[1]
+        r,pit,y = p.getEulerFromQuaternion(quat)
+        
+        AMatrix3 = np.array([
+            [1,0,np.sin(pit)],
+            [0,np.cos(r),-np.cos(pit)*np.sin(r)],
+            [0,np.sin(r),np.cos(pit)*np.cos(r)]
+        ])
+        #print(AMatrix3)
+        AMatrix6 = np.block([
+            [np.eye(3),       np.zeros((3,3))],
+            [np.zeros((3,3)),   AMatrix3]
+        ])
+        
+        return AMatrix6
+        
     
     def getDynamicMatrices(self):
         
@@ -96,17 +129,25 @@ class UR5manipulator():
         coriolisMatrix = np.array(p.calculateInverseDynamics(self.robotID,jointAngles,jointVel,zero)) - gravityMatrix
                 
         return massMatrix,gravityMatrix,coriolisMatrix
-        
-        
-        
-        
-
-            
-            
-
-        
-    def do_nothing(self):
-        pass
+    
+    
+    def ForceGUI(self, forces, max_limit = 10, min_limit = -10):
+        fxId = p.addUserDebugParameter("fx", min_limit, max_limit, forces[0]) #force along x
+        fyId = p.addUserDebugParameter("fy", min_limit, max_limit, forces[1]) #force along y
+        fzId = p.addUserDebugParameter("fz", min_limit, max_limit, forces[2]) #force along z
+        mxId = p.addUserDebugParameter("mx", min_limit, max_limit, forces[3]) #moment along x
+        myId = p.addUserDebugParameter("my", min_limit, max_limit, forces[4]) #moment along y
+        mzId = p.addUserDebugParameter("mz", min_limit, max_limit, forces[5]) #moment along z
+        return [fxId, fyId, fzId, mxId, myId, mzId]
+    
+    def readGUIparams(self, ids):
+        val1 = p.readUserDebugParameter(ids[0])
+        val2 = p.readUserDebugParameter(ids[1])
+        val3 = p.readUserDebugParameter(ids[2])
+        val4 = p.readUserDebugParameter(ids[3])
+        val5 = p.readUserDebugParameter(ids[4])
+        val6 = p.readUserDebugParameter(ids[5])
+        return np.array([val1, val2, val3, val4, val5, val6])   
         
     
         
@@ -117,15 +158,15 @@ if __name__ == "__main__":
     free fall
     '''
     
-    # ur5 = UR5manipulator()
+    ur5 = UR5manipulator()
     
-    # p.setJointMotorControlArray(ur5.robotID,ur5.controlableJoints,p.VELOCITY_CONTROL,forces = [0]*6)
+    p.setJointMotorControlArray(ur5.robotID,ur5.controlableJoints,p.VELOCITY_CONTROL,forces = [0]*6)
     
-    # while p.isConnected():
-    #     time.sleep(0.01)
-    #      ## should do free fall
-    #     p.setJointMotorControlArray(ur5.robotID,ur5.controlableJoints,p.TORQUE_CONTROL,forces = [0]*6)
-    #     p.stepSimulation()
+    while p.isConnected():
+        time.sleep(0.01)
+         ## should do free fall
+        p.setJointMotorControlArray(ur5.robotID,ur5.controlableJoints,p.TORQUE_CONTROL,forces = np.zeros(6))
+        p.stepSimulation()
     
     '''
     velocity control
@@ -164,70 +205,70 @@ if __name__ == "__main__":
     ####
     ##  IMPEDANCE CONTROL DIDN'T WORK
     
-    '''
-    impedance control. 
-           
-    ur5 = UR5manipulator()
+    # '''
+    # impedance control. 
+    # '''       
+    # ur5 = UR5manipulator()
     
     
-    initJointAngles = [0.5, -0.707, 1.0, -1.57, -1.57, -1.57]
-    ur5.setJointAngles(initJointAngles)
-    #time.sleep(5)
+    # initJointAngles = [0.5, -0.707, 1.0, -1.57, -1.57, -1.57]
+    # ur5.setJointAngles(initJointAngles)
+    # #time.sleep(5)
 
-    desiredEndEffector = ur5.getForwardKinematics()
-    desiredEndEffector = np.hstack((desiredEndEffector[0],desiredEndEffector[1]))
-    p.setJointMotorControlArray(ur5.robotID,ur5.controlableJoints,p.VELOCITY_CONTROL,forces = [0]*6)
+    # desiredEndEffector = ur5.getForwardKinematics()
+    # desiredEndEffector = np.hstack((desiredEndEffector[1],desiredEndEffector[0]))
+    # p.setJointMotorControlArray(ur5.robotID,ur5.controlableJoints,p.VELOCITY_CONTROL,forces = [0]*6)
     
-    for i in range(ur5.numJoints):
-        p.changeDynamics(ur5.robotID, i, linearDamping=0.0, angularDamping=0.0, jointDamping=0.0)
+    # for i in range(ur5.numJoints):
+    #     p.changeDynamics(ur5.robotID, i, linearDamping=0.0, angularDamping=0.0, jointDamping=0.0)
         
-    kMatrix = np.diag([10,10,10,1,1,1])
-    cMatrix = np.diag([0,0,0,0,0,0])
+    # kMatrix = np.diag([10,10,10,10,10,10])
+    # cMatrix = np.diag([100,100,100,100,100,100])
         
-    while True:
-        jointInfo = p.getJointStates(ur5.robotID,ur5.controlableJoints)
-        jointAngles ,jointVel = [],[]
+    # while True:
+    #     jointInfo = p.getJointStates(ur5.robotID,ur5.controlableJoints)
+    #     jointAngles ,jointVel = [],[]
         
-        for i in range(len(ur5.controlableJoints)):
-            jointAngles.append(jointInfo[i][0])
-            jointVel.append(jointInfo[i][1])
+    #     for i in range(len(ur5.controlableJoints)):
+    #         jointAngles.append(jointInfo[i][0])
+    #         jointVel.append(jointInfo[i][1])
         
-        pos,Quat = ur5.getForwardKinematics()
-        euler = Quat
-        endEffector = np.hstack((pos,euler))
-        endEffectorVel = np.dot(ur5.calculateJacobian(jointAngles) , jointVel) 
+    #     pos,Quat = ur5.getForwardKinematics()
+    #     euler = Quat
+    #     endEffector = np.hstack((euler,pos))
+    #     endEffectorVel = np.dot(ur5.calculateJacobian(jointAngles) , jointVel) 
         
-        deltaX = -endEffector + desiredEndEffector
+    #     deltaX = -endEffector + desiredEndEffector
         
-        threshHold = 1e-6
-        indices = deltaX < threshHold
-        deltaX[indices] = 0
+    #     threshHold = 1e-6
+    #     indices = deltaX < threshHold
+    #     deltaX[indices] = 0
         
         
-        print("deltaX",deltaX)
+    #     print("deltaX",deltaX)
         
-        deltaV = -endEffectorVel
-        indices = deltaV < threshHold
-        deltaV[indices] = 0
-        print("DeltaV",deltaV)
+    #     deltaV = -endEffectorVel
+    #     indices = deltaV < threshHold
+    #     deltaV[indices] = 0
+    #     print("DeltaV",deltaV)
         
-        # springTorque = np.linalg.multi_dot((ur5.calculateJacobian(jointAngles).T,kMatrix,deltaX))
-        # dampingTorque = np.linalg.multi_dot((ur5.calculateJacobian(jointAngles).T,cMatrix,deltaV))
+    #     # springTorque = np.linalg.multi_dot((ur5.calculateJacobian(jointAngles).T,kMatrix,deltaX))
+    #     # dampingTorque = np.linalg.multi_dot((ur5.calculateJacobian(jointAngles).T,cMatrix,deltaV))
         
-        springForce = np.dot(kMatrix,deltaX)
-        print("springForce:",springForce)
+    #     springForce = np.dot(kMatrix,deltaX)
+    #     print("springForce:",springForce)
         
-        dampingForce = np.dot(cMatrix,deltaV)
-        print("dampingForce",dampingForce)
-        spatialImpedance = springForce + dampingForce
-        jointImpedance = np.dot(ur5.calculateJacobian(jointAngles).T, spatialImpedance)
-        print("jointImpedance:",jointImpedance)
+    #     dampingForce = np.dot(cMatrix,deltaV)
+    #     print("dampingForce",dampingForce)
+    #     spatialImpedance = springForce + dampingForce
+    #     jointImpedance = np.dot(ur5.calculateJacobian(jointAngles).T, spatialImpedance)
+    #     print("jointImpedance:",jointImpedance)
         
-        massMatrix,gravityMatrix,coriolisMatrix = ur5.getDynamicMatrices()
-        torque = gravityMatrix + jointImpedance
-        p.setJointMotorControlArray(ur5.robotID,ur5.controlableJoints,p.TORQUE_CONTROL,forces = torque)
-        p.stepSimulation()
-        '''
+    #     massMatrix,gravityMatrix,coriolisMatrix = ur5.getDynamicMatrices()
+    #     torque = gravityMatrix + jointImpedance
+    #     p.setJointMotorControlArray(ur5.robotID,ur5.controlableJoints,p.TORQUE_CONTROL,forces = torque)
+    #     p.stepSimulation()
+        
     
     
     pass
