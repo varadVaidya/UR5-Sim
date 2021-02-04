950ef69f84a4d9fa612ac82014e05bc8e5ab0ba7
Varad <vaidyavarad2001@gmail.com>
2021-02-04 08:34:53 +0530

    current impedance control didn't work trying  new one

--- impedance.py
+++ impedance.py
@@ -4,15 +4,17 @@ import numpy as np
 from math import sqrt
 from UR5 import UR5manipulator
 
+###################
+#########
+### WRONG IMPLEMENTAION.###########
+
 #### The Gui forces are the forces experienced by the end effector
 
 arm = UR5manipulator()          # setting up the manipulator
-
+p.setRealTimeSimulation(0)
 ## set init joint anglees and positon robot their
-initJointAngles = [0.5, -0.707,0.5, 0.1, 0, 0]
+initJointAngles = [0.7, -0.707,0.5, 0.1, 0, 0]
 arm.setJointAngles(initJointAngles)
-
-arm.turnOFFJointControl()       # turn off joint control to apply torque
 
 ## find the gravity matrix for the start. 
 massMatrix,gravityMatrix,coriolisMatrix = arm.getDynamicMatrices()
@@ -27,18 +29,16 @@ print("Xdes:",xDes)
 for i in range(arm.numJoints):
     p.changeDynamics(arm.robotID, i, linearDamping=0.0, angularDamping=0.0, jointDamping=0.0)
 
-Kp = 2 * np.eye(6)
-Kd = 1 * np.eye(6)
+Kp = np.diag(5*[0.1],k=-1)+np.diag(6*[1.5],k=0)+np.diag(5*[0.1],k=1)
+Kd = np.diag(5*[0.1],k=-1)+np.diag(6*[1.2],k=0)+np.diag(5*[0.1],k=1)
 externalForce = np.array([0,0,0,0,0,0])
 GUIForce = arm.ForceGUI(externalForce)
+arm.turnOFFJointControl()       # turn off joint control to apply torque
 
 while True:
     
-    
     externalForce = arm.readGUIparams(GUIForce)
-    p.applyExternalForce(arm.robotID,arm.endEffectorLink,externalForce[0:3],[0,0,0],flags=p.LINK_FRAME)
-    p.applyExternalTorque(arm.robotID,arm.endEffectorLink,externalForce[3:6],p.LINK_FRAME)
-    
+  
     #forward dymanics
     jointAngles,jointVelocities,jointTorques = arm.getJointStates()
     #print("jointAngles are:",jointAngles)
@@ -59,20 +59,18 @@ while True:
     
     ## calculate current end effector position
     currentX = np.array(arm.getForwardKinematics()).reshape(6)
-    #print("currentX:",currentX)
-    #input("checck")
-    
-    
     deltaX = xDes - currentX    
     
     aMatrix = arm.calculateAnalyticalVelocityMap()
     appliedForce = np.linalg.multi_dot(( np.linalg.inv(aMatrix).T , Kp ,deltaX)) - \
-        np.linalg.multi_dot((Kd,jacobian,jointVelocities)) + spatialGravity 
-    
+        np.linalg.multi_dot((Kd,jacobian,jointVelocities)) + spatialGravity
+    #appliedForce = np.linalg.multi_dot((Kp,deltaX)) - np.linalg.multi_dot((Kd,np.linalg.inv(aMatrix),jacobian,jointVelocities)) + gravityMatrix
     torque = np.dot(jacobian.T,appliedForce) 
     
+    p.applyExternalForce(arm.robotID,arm.endEffectorLink,externalForce[0:3],[0,0,0],flags=p.LINK_FRAME)
+    p.applyExternalTorque(arm.robotID,arm.endEffectorLink,externalForce[3:6],p.LINK_FRAME)  
     p.setJointMotorControlArray(arm.robotID,arm.controlableJoints,controlMode = p.TORQUE_CONTROL,forces=torque) 
-    p.stepSimulation()     
+    p.stepSimulation()
 
     #p.setJointMotorControlArray(arm.robotID,arm.controlableJoints,controlMode = p.TORQUE_CONTROL,forces=torque)
 
